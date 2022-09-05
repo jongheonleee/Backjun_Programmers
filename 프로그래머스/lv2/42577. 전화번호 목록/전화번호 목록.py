6197620faad28f95b24a6dd022aa7030955df772
yeonuel <87258372+jongheonleee@users.noreply.github.com>
2022-09-05 17:39:51 +0900

    [level 2] Title: 전화번호 목록, Time: 186.00 ms, Memory: 34.5 MB -BaekjoonHub

--- 프로그래머스/lv2/42577. 전화번호 목록/전화번호 목록.py
+++ 프로그래머스/lv2/42577. 전화번호 목록/전화번호 목록.py
@@ -1,11 +1,8 @@
 def solution(phone_book):
-    # 한 번호가 다른 번호의 접두어인 경우가 있는지 확인하려 합니다.
-    # 어떤 번호가 다른 번호의 접두어인 경우가 있으면 false,
-    # 그렇지 않으면 true를 return 하도록 solution 함수를 작성해주세요.
+    hash_map = {}
     
-    phone_num = {} 
     for num in phone_book:
-        phone_num[num] = 1
+        hash_map[num] = 1
         
     for num in phone_book:
         tmp = ''
@@ -13,8 +10,9 @@ def solution(phone_book):
         for n in num:
             tmp += n
             
-            if tmp in phone_num and tmp != num:
+            if tmp in hash_map and tmp != num:
                 return False
-            
+                
     return True
+    
     
