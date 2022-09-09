74c01e0da62f8cc4309261c2a0e1b6acd9b49988
yeonuel <87258372+jongheonleee@users.noreply.github.com>
2022-09-09 23:21:24 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 4.44 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
@@ -0,0 +1,27 @@
+import collections
+def solution(maps):
+    if len(maps) == 1:
+        if 0 in maps:
+            return -1
+        
+    if len(maps[0]) == 1:
+        if 0 in maps:
+            return -1
+
+    row, col = len(maps), len(maps[0])
+    queue = collections.deque([(0, 0)])
+    d = [(1,0), (0,1), (-1,0), (0, -1)]
+
+    
+    while queue:
+        y, x = queue.popleft()
+        
+        for dy, dx in d:
+            ny, nx = y+dy, x+dx
+            
+            if 0<= ny<=row-1 and 0<= nx<=col-1 and maps[ny][nx] == 1:
+                maps[ny][nx] = maps[y][x] + 1
+                queue.append((ny, nx))
+                
+    return maps[row-1][col-1] if maps[row-1][col-1] > 1 else -1
+                        
