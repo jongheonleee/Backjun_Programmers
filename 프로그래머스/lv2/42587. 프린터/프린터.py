33de5f7a42225f96d1bd7ee813f607474c3c48c9
yeonuel <87258372+jongheonleee@users.noreply.github.com>
2022-09-09 12:52:31 +0900

    [level 2] Title: 프린터, Time: 0.06 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv2/42587. 프린터/프린터.py
+++ 프로그래머스/lv2/42587. 프린터/프린터.py
@@ -3,7 +3,6 @@ def solution(priorities, location):
     # 우선순위 큐 구현 x -> 우선순위가 같으면 인덱스 작은것 부터 반환함
     ans = 0
     queue = collections.deque()
-    seen = set()
     
     for i, p in enumerate(priorities):
         queue.append((i, p))
@@ -11,23 +10,17 @@ def solution(priorities, location):
     while 1:
         max_p = max(queue, key=lambda x:x[1])
         
-        
+        # 우선순위 기준으로 프린트 순서 재배열
         for _ in range(len(queue)):
             if queue[0][1] == max_p[1]:
                 break
                 
             else:
                 idx, cand = queue.popleft()
-                queue.append((idx, cand))
-                
-            print(queue)
-                
+                queue.append((idx, cand))        
 
         loc, p = queue.popleft()
         ans += 1
         
         if loc == location:
             return ans
-        
-    return ans
-        
