1b4f53e3e96e8a65d08f83f1e3ec89f0183813e7
yeonuel <87258372+jongheonleee@users.noreply.github.com>
2022-09-10 18:35:04 +0900

    [level 3] Title: 이중우선순위큐, Time: 0.03 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv3/42628. 이중우선순위큐/이중우선순위큐.py
+++ 프로그래머스/lv3/42628. 이중우선순위큐/이중우선순위큐.py
@@ -5,15 +5,15 @@ def solution(operations):
     
     # max_heap구현
     for o in operations:
-        if 'I' in o:
+        if 'I' == o[0]:
             heapq.heappush(max_heap, -int(o[2:]))
             heapq.heappush(min_heap, int(o[2:]))
             
-        if 'D 1' in o and max_heap:
+        if 'D 1' == o[:3] and max_heap:
             max_num = heapq.heappop(max_heap)
             min_heap.remove(-max_num)
             
-        if 'D -1' in o and min_heap:
+        if 'D -1' == o[:4] and min_heap:
             min_num = heapq.heappop(min_heap)
             max_heap.remove(-min_num)
             
