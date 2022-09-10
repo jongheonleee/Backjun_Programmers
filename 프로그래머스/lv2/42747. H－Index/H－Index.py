1ffdfaaa92bf94b7279641921399ba16a49858b5
yeonuel <87258372+jongheonleee@users.noreply.github.com>
2022-09-10 17:08:21 +0900

    [level 2] Title: H-Index, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv2/42747. H－Index/H－Index.py
+++ 프로그래머스/lv2/42747. H－Index/H－Index.py
@@ -1,14 +1,4 @@
 def solution(citations):
-#     max_c = 0
-#     citations.sort()
-    
-#     for i, c in enumerate(citations):
-#         cnt_cite = len(citations)-i
-        
-#         if i+1 <= c <= cnt_cite:
-#             max_c = max(max_c, c)
-        
-#     return max_c
     citations.sort()
     for i, c in enumerate(citations):
         if c >= len(citations) - i:
