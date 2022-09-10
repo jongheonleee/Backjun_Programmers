11f2658da32051b6dd1ecdce9ba4907f7058eb05
yeonuel <87258372+jongheonleee@users.noreply.github.com>
2022-09-10 17:21:22 +0900

    [level 2] Title: 가장 큰 수, Time: 0.03 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv2/42746. 가장 큰 수/가장 큰 수.py
+++ 프로그래머스/lv2/42746. 가장 큰 수/가장 큰 수.py
@@ -1,6 +1,6 @@
 def solution(numbers):
     str_num = list(map(str, numbers))
-    str_num.sort(key=lambda x: x*3, reverse=True)
+    str_num.sort(key=lambda x: x*5, reverse=True)
     
     
     return str(int(''.join(str_num)))
