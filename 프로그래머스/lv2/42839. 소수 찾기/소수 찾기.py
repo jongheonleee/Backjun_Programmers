f3c3ee853d77b4a36a6acf0334eb1a92edc54cca
yeonuel <87258372+jongheonleee@users.noreply.github.com>
2022-09-12 20:52:59 +0900

    [level 2] Title: 소수 찾기, Time: 0.48 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/lv2/42839. 소수 찾기/소수 찾기.py
+++ 프로그래머스/lv2/42839. 소수 찾기/소수 찾기.py
@@ -3,22 +3,31 @@ def solution(numbers):
     ans, nums, per = [], [n for n in numbers], []
     
     for i in range(1, len(numbers)+1):
+        # nums에서 i개씩 순열 조합 만들어주기
         per += list(permutations(nums, i))
         
+    # 각 순열 조합을 하나의 문자열로 만들어서 리스트에 저장
     new_nums = [int(("").join(p)) for p in per]
     
+    # 모든 순열 조합의 숫자들
     for n in new_nums:
         if n < 2:
             continue
             
         check = True
         
+        # 소수인지 아닌지 판별
+        # n의 약수의 절반
+        # 각 숫자들로 나눠보기
         for i in range(2, int(n**0.5)+1):
+            # 나누어 떨어지면 소수가 아님
             if n % i == 0:
                 check = False
                 break
                 
+        # 소수일 경우 해당 숫자 ans에 담기
         if check:
             ans.append(n)
             
+    # 같은 숫자가 순열에 담겨있는 경우가 있음
     return len(set(ans))
