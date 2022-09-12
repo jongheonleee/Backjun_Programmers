d59fd52e4c7591cba1f10e27d41290ecccfd0bfc
yeonuel <87258372+jongheonleee@users.noreply.github.com>
2022-09-12 15:10:04 +0900

    [level 3] Title: 단어 변환, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv3/43163. 단어 변환/단어 변환.py
@@ -0,0 +1,48 @@
+import math
+import collections
+def solution(begin, target, words):
+    answer = math.inf 
+
+    similar_map = collections.defaultdict(list)
+    similar_map[begin] = []
+    for w in words:
+        match_cnt = 0
+        
+        for i in range(len(w)):
+            if w[i] == begin[i]:
+                match_cnt += 1
+                
+        if match_cnt == len(w) - 1:
+            similar_map[begin] += [w]
+            
+    for w1 in words:
+        for w2 in words:
+            if w1 == w2 or begin in [w1, w2]:
+                continue
+                
+            match_cnt = 0
+            for i in range(len(w1)):
+                if w1[i] == w2[i]:
+                    match_cnt += 1
+                    
+            if match_cnt == len(w1)-1:
+                similar_map[w1] += [w2]
+                
+                
+    def dfs(word, cnt, seen):
+        if word == target:
+            nonlocal answer
+            answer = min(cnt, answer)
+            return
+        
+        if word != target and cnt >= len(words):
+            return 
+        
+        seen.add(word)
+        for nw in similar_map[word]:
+            if nw not in seen:
+                dfs(nw, cnt+1, seen)
+                 
+        
+    dfs(begin, 0, set())  
+    return answer if answer != math.inf else 0
