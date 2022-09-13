79af2ade4949b28c00fd3c80772f7e4b3480d561
yeonuel <87258372+jongheonleee@users.noreply.github.com>
2022-09-13 20:39:50 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 1.45 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.py
+++ 프로그래머스/lv2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.py
@@ -1,39 +1,35 @@
-import collections, math
+import math, collections
 def solution(n, wires):
-    # 전선들 중 하나를 끊어서 송전탑 개수가 가능한 비슷하도록 두 전력망으로 나누
-    # 개수의 차이(절대값)를 return
     ans = math.inf
-    graph = collections.defaultdict(set)
     
-    def bfs(w):
+    def bfs(w) -> int:
         cnt, vis = 1, [False] * (n+1)
-        # 방문 기록을 리스트에서 각 인덱스의 True/False로 저장
-        # - True: 방문
-        # - False: 미방문
         vis[w[0]] = True
         queue = collections.deque([w[0]])
         
         while queue:
             crnt_v = queue.popleft()
             
-            for i in graph[crnt_v]:
-                if vis[i] or i == w[1]:
+            for next_v in graph[crnt_v]:
+                if next_v == w[1] or vis[next_v]:
                     continue
                     
+                queue.append(next_v)
+                vis[next_v] = True
                 cnt += 1
-                queue.append(i)
-                vis[i] = True
                 
         return cnt
     
-    # graph만들기 방향x
-    for v1, v2 in wires:
-        graph[v1].add(v2)
-        graph[v2].add(v1)
+    graph = collections.defaultdict(set)
+    for w1, w2 in wires:
+        graph[w1].add(w2)
+        graph[w2].add(w1)
         
-    # w:[v1, v2] 끊어진 노드를 의미함. 
     for w in wires:
         cnt = bfs(w)
-        # A: n-cnt, b: cnt
-        ans = min(ans, abs(n-cnt*2))
+        ans = min(ans, abs(n - 2*cnt))
+        
     return ans
+        
+    
+    
