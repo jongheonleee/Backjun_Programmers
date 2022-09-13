ee29ca3cf71c825a33d5e70ff41e30d11c8de964
yeonuel <87258372+jongheonleee@users.noreply.github.com>
2022-09-14 08:34:45 +0900

    [level 3] Title: 아이템 줍기, Time: 0.25 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/lv3/87694. 아이템 줍기/아이템 줍기.py
+++ 프로그래머스/lv3/87694. 아이템 줍기/아이템 줍기.py
@@ -1,43 +1,38 @@
-import collections 
+import collections
 def solution(rectangle, characterX, characterY, itemX, itemY):
     ans, MAX = 0, 102
+    coordinate = [[-1] * MAX for _ in range(MAX)]
     
-    field = [[5] * MAX for _ in range(MAX)]
-    
-    # 테두리 그리기
-    # 두배해주는 이유
-    # 입출력 예1에서, 현재 좌표가 (3,5)일때 위로 이동하면 (3,6)도 성립
-    # 하지만 실제로는 테두리가 이어져 있지않아서 이동 불가함
     for rect in rectangle:
         x1, y1, x2, y2 = map(lambda x:x*2, rect)
         
         for i in range(x1, x2+1):
             for j in range(y1, y2+1):
                 if x1 < i < x2 and y1 < j < y2:
-                    field[i][j] = 0
+                    coordinate[i][j] = 0
                     
-                elif field[i][j] != 0:
-                    field[i][j] = 1
+                elif coordinate[i][j] != 0:
+                    coordinate[i][j] = 1
                     
     queue = collections.deque()
-    queue.append([characterX * 2, characterY * 2])
+    queue.append((characterX * 2, characterY * 2))
     vis = [[0] * MAX for _ in range(MAX)]
-    vis[characterX * 2][characterY * 2] = 1
-    d_xy = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+    vis[characterX*2][characterY*2] = 1
+    d_xy = [(-1,0), (1,0), (0,-1), (0,1)]
     
     while queue:
-        x, y = queue.popleft()
+        crnt_x, crnt_y = queue.popleft()
         
-        if x == itemX * 2 and y == itemY * 2:
-            ans = (vis[x][y] - 1) // 2
+        if crnt_x == itemX * 2 and crnt_y == itemY * 2:
+            ans = (vis[crnt_x][crnt_y] - 1)//2
             break
-            
+        
         for dx, dy in d_xy:
-            nx, ny = x+dx, y+dy
+            nx, ny = crnt_x+dx, crnt_y+dy
             
-            if vis[nx][ny] == 0 and field[nx][ny] == 1:
-                queue.append([nx, ny])
-                vis[nx][ny] = vis[x][y] + 1
+            if vis[nx][ny] == 0 and coordinate[nx][ny] == 1:
+                queue.append((nx, ny))
+                vis[nx][ny] = vis[crnt_x][crnt_y] + 1
                 
     return ans
-    
+                
