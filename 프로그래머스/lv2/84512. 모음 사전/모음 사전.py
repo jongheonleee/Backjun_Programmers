66d71fe5daf7e61fe35f55f98b40f2e2e286d895
yeonuel <87258372+jongheonleee@users.noreply.github.com>
2022-09-13 21:51:49 +0900

    [level 2] Title: 모음 사전, Time: 1.19 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv2/84512. 모음 사전/모음 사전.py
+++ 프로그래머스/lv2/84512. 모음 사전/모음 사전.py
@@ -1,5 +1,15 @@
+from itertools import product
 def solution(word):
-    ans = 0
-    for i, n in enumerate(word):
-        ans += (5 ** (5-i) -1) / (5-1) * 'AEIOU'.index(n) + 1 
-    return ans
+    words = []
+    
+    # 중복 순열활용하기
+    # words의 길이가 5이하
+    for i in range(1, 6):
+        for c in product(['A', 'E', 'I', 'O', 'U'], repeat=i):
+            # 이때, c 같은 경우는 중복조합(튜플형태임)
+            # 문자열로 전환해서 리스트에 담기
+            words.append(''.join(list(c)))
+    
+    # 정렬
+    words.sort()
+    return words.index(word) + 1
