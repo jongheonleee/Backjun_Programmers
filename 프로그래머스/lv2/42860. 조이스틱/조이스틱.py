87478a24c6ec7b197cffcb32a21c5feae0c1b399
yeonuel <87258372+jongheonleee@users.noreply.github.com>
2022-09-14 20:52:03 +0900

    [level 2] Title: 조이스틱, Time: 0.01 ms, Memory: 10 MB -BaekjoonHub

--- 프로그래머스/lv2/42860. 조이스틱/조이스틱.py
+++ 프로그래머스/lv2/42860. 조이스틱/조이스틱.py
@@ -1,16 +1,16 @@
 def solution(name):
-    # 이름에 대해 조이스틱 조작 횟수의 최솟값을 return 
-    ans = 0
-    min_move = len(name)-1
+    ans, min_move = 0, len(name)-1
     
     for i, char in enumerate(name):
-        ans += min(ord(char) - ord('A'), ord('Z') - ord(char) + 1)
+        ans += min(ord(char)-ord('A'), ord('Z')-ord(char)+1)
         
-        nt = i+1
-        while nt < len(name) and name[nt] == 'A':
-            nt += 1
+        j = i+1
+        while j < len(name) and name[j] == 'A':
+            j += 1
             
-        min_move = min(min_move, 2 * i + len(name) - nt, i + 2 * (len(name) - nt))
+        min_move = min([min_move, 2*i+(len(name)-j), i+2*(len(name)-j)])
         
     ans += min_move
+    return ans
+    
     return ans
