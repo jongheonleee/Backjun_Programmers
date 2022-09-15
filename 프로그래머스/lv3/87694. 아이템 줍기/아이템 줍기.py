e23afa8003040f3145f45290bac0bf9aeec1351f
yeonuel <87258372+jongheonleee@users.noreply.github.com>
2022-09-15 20:03:37 +0900

    [level 3] Title: 아이템 줍기, Time: 0.41 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/lv3/87694. 아이템 줍기/아이템 줍기.py
+++ 프로그래머스/lv3/87694. 아이템 줍기/아이템 줍기.py
@@ -1,39 +1,38 @@
 import collections
 def solution(rectangle, characterX, characterY, itemX, itemY):
-    answer = 0
-    MAX = 102
-    field = [[-1] * MAX for _ in range(MAX)]
-    d = [(1,0),(-1,0),(0,1),(0,-1)]
+    ans, MAX = 0, 102
+    coordinate = [[-1] * MAX for _ in range(MAX)]
     
     for rect in rectangle:
-        x1, y1, x2, y2 = map(lambda x:x*2,rect)
+        x1, y1, x2, y2 = map(lambda x:x*2, rect)
         
         for i in range(x1, x2+1):
             for j in range(y1, y2+1):
                 if x1 < i < x2 and y1 < j < y2:
-                    field[i][j] = 0
+                    coordinate[i][j] = 0
                     
-                elif field[i][j] != 0:
-                    field[i][j] = 1
-    
-    queue = collections.deque([])
-    queue.append((characterX*2, characterY*2))
-    vis = [[0]*MAX for _ in range(MAX)]
+                elif coordinate[i][j] != 0:
+                    coordinate[i][j] = 1
+                    
+    queue = collections.deque()
+    queue.append((characterX * 2, characterY * 2))
+    vis = [[0] * MAX for _ in range(MAX)]
     vis[characterX*2][characterY*2] = 1
-    
+    d_xy = [(-1,0), (1,0), (0,-1), (0,1)]
     
     while queue:
         crnt_x, crnt_y = queue.popleft()
         
         if crnt_x == itemX * 2 and crnt_y == itemY * 2:
-            answer = (vis[crnt_x][crnt_y] - 1)//2
+            ans = (vis[crnt_x][crnt_y] - 1)//2
             break
         
-        for dx, dy in d:
+        for dx, dy in d_xy:
             nx, ny = crnt_x+dx, crnt_y+dy
             
-            if vis[nx][ny] == 0 and field[nx][ny] == 1:
+            if vis[nx][ny] == 0 and coordinate[nx][ny] == 1:
                 queue.append((nx, ny))
-                vis[nx][ny] = 1 + vis[crnt_x][crnt_y]
-        
-    return answer
+                vis[nx][ny] = vis[crnt_x][crnt_y] + 1
+                
+    return ans
+                
