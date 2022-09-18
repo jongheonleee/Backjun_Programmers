bcfb71a18788b70e31f194df2e3310d2460f86ad
yeonuel <87258372+jongheonleee@users.noreply.github.com>
2022-09-18 17:10:09 +0900

    [level 3] Title: 단속카메라, Time: 0.92 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv3/42884. 단속카메라/단속카메라.py
+++ 프로그래머스/lv3/42884. 단속카메라/단속카메라.py
@@ -1,15 +1,12 @@
 def solution(routes):
-    # routes를 나간시간 순으로 오름차순 정렬
-    routes.sort(key = lambda x: x[1])
     answer = 1
-    # camera = 현재 카메라가 설치된 위치
-    camera = routes[0][1]
+    routes.sort(key=lambda x:x[1])
+    cctv = routes[0][1]
     
-    # 두번째 차량부터 마지막번째 차량까지 반복문을 돌며 현재 카메라가 설치된 시간보다
-    # 들어온 시간이 늦으면 camera에 현재 차량의 나간시간을 넣어주고 answer 1증가
-    for i in range(1, len(routes)):
-        if camera < routes[i][0]:
-            camera = routes[i][1]
+    for start, end in routes[1:]:
+        if cctv < start:
             answer += 1
-                
+            cctv = end
+            
+    
     return answer
