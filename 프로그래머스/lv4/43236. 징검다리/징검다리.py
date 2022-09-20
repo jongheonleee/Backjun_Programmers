06027efbd5ea26bff6d91a98d609f1acd09f4fac
yeonuel <87258372+jongheonleee@users.noreply.github.com>
2022-09-20 20:43:42 +0900

    [level 4] Title: 징검다리, Time: 0.02 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv4/43236. 징검다리/징검다리.py
+++ 프로그래머스/lv4/43236. 징검다리/징검다리.py
@@ -6,7 +6,7 @@ def solution(distance, rocks, n):
     left, right = 0, distance
     while left <= right:
         mid = (left + right) // 2
-        mid_dis = float('inf')
+        min_dis = float('inf')
         
         crnt, cnt_del = 0, 0
         
@@ -18,13 +18,13 @@ def solution(distance, rocks, n):
                 
             else:
                 crnt = r
-                mid_dis = min(mid_dis, diff)
+                min_dis = min(min_dis, diff)
                 
         if cnt_del > n:
             right = mid - 1
             
         else:
-            ans = mid_dis
+            ans = min_dis
             left = mid + 1
             
     return ans
