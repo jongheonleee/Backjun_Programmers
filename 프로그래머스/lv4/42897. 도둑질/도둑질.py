af6d14c167662726448906500330e178b31e511e
yeonuel <87258372+jongheonleee@users.noreply.github.com>
2022-09-22 00:17:35 +0900

    [level 4] Title: 도둑질, Time: 529.85 ms, Memory: 88.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv4/42897. 도둑질/도둑질.py
@@ -0,0 +1,15 @@
+def solution(money):
+    dp1, dp2 = [0] * len(money), [0] * len(money)
+    
+    # 1번 집을 터는 경우
+    dp1[0] = money[0]
+    for i in range(1, len(money) -1):
+        # 마지막 집은 못 텀
+        dp1[i] = max(dp1[i-1], dp1[i-2] + money[i])
+        
+    # 1번 집을 안터는 경우    
+    dp1[0] = 0
+    for j in range(1, len(money)):
+        dp2[j] = max(dp2[j-1], dp2[j-2] + money[j])
+        
+    return max(dp1[-2], dp2[-1])
