dab26dba9f04fbf91c42fc8916ca8079901e541a
yeonuel <87258372+jongheonleee@users.noreply.github.com>
2022-09-22 09:37:37 +0900

    [level 4] Title: 사칙연산, Time: 0.21 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv4/1843. 사칙연산/사칙연산.py
+++ 프로그래머스/lv4/1843. 사칙연산/사칙연산.py
@@ -1,18 +1,24 @@
 def solution(arr):
-    minmax = [0, 0]
-    sum_value = 0
-    for idx in range(len(arr)-1, -1, -1):
-        if arr[idx] == '+':
+    dp = [0, 0]
+    # dp[0]:min, dp[1]:max
+    cum_sum = 0
+    
+    for i in range(len(arr)-1, -1, -1):
+        if arr[i] == '+':
             continue
-        elif arr[idx] == '-':
-            tempmin, tempmax = minmax
-            minmax[0] = min(-(sum_value + tempmax), -sum_value+tempmin)
-            # -(sum + max):-가 식전체에 붙는 경우, -sum+min:-가 이전 -값 앞까지만 붙는 경우
-            minus_v = int(arr[idx+1])
-            minmax[1] = max(-(sum_value+tempmin), -minus_v+(sum_value-minus_v)+tempmax)
-            # -(sum + min):-가 식전체에 붙는 경우, -v+(sum-v)+max:-가 바로 뒤의 값에만 붙는 경우
-            sum_value = 0
-        elif int(arr[idx]) >= 0:
-            sum_value += int(arr[idx])
-    minmax[1] += sum_value
-    return minmax[1]
+            
+        elif arr[i] == '-':
+            sub_min, sub_max = dp
+            
+            dp[0] = min(-(cum_sum+sub_max), -cum_sum +sub_min)
+            
+            minus_num = int(arr[i+1])
+            dp[1] = max(-(cum_sum+sub_min), -minus_num+(cum_sum-minus_num) + sub_max)
+            
+            cum_sum = 0
+            
+        
+        else:
+            cum_sum += int(arr[i])
+            
+    return dp[1] + cum_sum
