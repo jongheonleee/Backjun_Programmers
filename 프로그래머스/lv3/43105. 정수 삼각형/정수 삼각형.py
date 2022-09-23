1fd3c9bd96944f1811906acc3452640a5e594fd3
yeonuel <87258372+jongheonleee@users.noreply.github.com>
2022-09-24 08:18:16 +0900

    [level 3] Title: 정수 삼각형, Time: 45.23 ms, Memory: 14.4 MB -BaekjoonHub

--- 프로그래머스/lv3/43105. 정수 삼각형/정수 삼각형.py
+++ 프로그래머스/lv3/43105. 정수 삼각형/정수 삼각형.py
@@ -1,12 +1,21 @@
-def solution(triangle):
-    for i in range(1, len(triangle)):
-        for j in range(0, i+1):
+def solution(dp):
+    # 삼각형의 정보가 담긴 배열 triangle이 매개변수
+    # 거쳐간 숫자의 최댓값을 return
+    
+    for i in range(1, len(dp)):
+        for j in range(len(dp[i])):
             if j == 0:
-                triangle[i][j] += triangle[i-1][j]
-            elif j == i:
-                triangle[i][j] += triangle[i-1][j-1]
+                dp[i][j] += dp[i-1][j]
+                
+            elif j == len(dp[i])-1:
+                dp[i][j] += dp[i-1][j-1]
+                
             else:
-                triangle[i][j] += max(triangle[i-1][j], triangle[i-1][j-1])
-
-    return max(triangle[-1])
-
+                dp[i][j] += max(dp[i-1][j], dp[i-1][j-1])
+                
+    return max(dp[-1])
+                
+                
+            
+    
+    return 
